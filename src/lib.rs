02a9e720c21aa2086489037e430beb770ef966cb
nagy135 <viktor.nagy@gmail.com>
2019-12-21 15:46:32 +0100

    lib

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod constants {
+    pub static PASSWORD_HASH_HOLDER: &'static str = "/home/infiniter/pass_hash";
+    pub const PASS_DELAY: u32 = 5;
+    pub static STORAGE: &'static str = "/home/infiniter/storage.db";
+}
